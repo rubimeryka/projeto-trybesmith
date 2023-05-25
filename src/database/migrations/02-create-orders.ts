422d8f5a7ec61f27dc9d8c0a06bdd6eb0fc4d8eb
rubimeryka <erykarubim13@gmail.com>
2023-05-25 17:49:19 -0300

    iniciando o projeto

--- src/database/migrations/02-create-orders.ts
+++ src/database/migrations/02-create-orders.ts
@@ -4,12 +4,14 @@ import { Order } from 'src/types/Order';
 export default {
   up(queryInterface: QueryInterface) {
     return queryInterface.createTable<Model<Order>>('orders', {
+
       id: {
         type: DataTypes.INTEGER.UNSIGNED,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
       },
+      
       userId: {
         type: DataTypes.INTEGER,
         allowNull: false,
